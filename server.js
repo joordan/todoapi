244c3338faeab0d2c0941ae5bce723a281a7a962
jooordan <jespejoo@yahoo.com>
2016-08-11 14:48:29 -0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -21,13 +22,15 @@ app.get('/todos', function (request,response) {
 // GET /todos/:id
 app.get('/todos/:id', function (request,response) {		//:id whatever user puts in
 	var todoId = parseInt(request.params.id,10);		// store whatever user put in to use for  
-	var matchedTodo;									//searching parse int for str to int conversion
+	var matchedTodo = _.findWhere(todos, {id: todoId}); // _.findWhere underscore refactoring. commonly used code functions
 
-	todos.forEach(function (todo) { 	//anonymous function called for each item or module
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	}); 	
+	// var matchedTodo;									//searching parse int for str to int conversion
+
+	// todos.forEach(function (todo) { 	//anonymous function called for each item or module
+	// 	if (todoId === todo.id) {
+	// 		matchedTodo = todo;
+	// 	}
+	// }); 	
 
 	if (matchedTodo) {
 		response.json(matchedTodo);
@@ -41,7 +44,13 @@ app.get('/todos/:id', function (request,response) {		//:id whatever user puts in
 
 // POST /todos access data sent by request by using npmmodule bodyparser
 app.post('/todos', function (request,response) {
-	var body = request.body;
+	var body = _.pick(request.body,'description','completed');	//_.pick only allows certain key values to be taken from json
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) { //test if body.
+		return response.status(400).send();	//	completed and body.string, trim removes whitespaces
+	}
+
+	body.description = body.description.trim();
 
 	// add id field
 	body.id = todoNextId++;
@@ -52,8 +61,6 @@ app.post('/todos', function (request,response) {
 	// console.log('description: ' + body.description );
 	response.json(body);
 });
-
-
 
 
 app.listen(PORT, function() {
