9a14ce5262a88813655183ae7e919ab92c2208b9
jooordan <jespejoo@yahoo.com>
2016-08-12 07:44:38 -0700

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -65,7 +65,6 @@ app.post('/todos', function (request,response) {
 // DELETE /todos/:id
 
 app.delete('/todos/:id', function (request, response) {
-	var todoId = parseInt(request.params.id,10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (!matchedTodo) {
@@ -74,6 +73,41 @@ app.delete('/todos/:id', function (request, response) {
 		todos = _.without(todos,matchedTodo);
 		response.json(matchedTodo);
 	}
+
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (request, response) {
+		var todoId = parseInt(request.params.id,10);
+		var matchedTodo = _.findWhere(todos, {id: todoId});
+		var body = _.pick(request.body,'description','completed');	//filter with _.pick only allows certain key values to be taken from json
+		var validAttributes = {};
+
+		if (!matchedTodo) {
+			return response.status(404).send();
+		}
+
+		//if it exist, it meets standards for completed
+		if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) { //if property exist and its a boolean
+			validAttributes.completed = body.completed;
+		} else if (body.hasOwnProperty('completed')) {
+			// bad happened
+			return response.status(400).send();
+		} 
+
+		//if it exist, description
+		if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+			validAttributes.description = body.description;
+		} else if (body.hasOwnProperty('description')) {
+			return response.status(400).send();
+		}
+
+		// things went right
+
+		_.extend(matchedTodo, validAttributes);
+
+		response.json(matchedTodo);//automatically send back status 200
+		
 
 });
 
